a1c76172ab1b711db5fb67988aba01d30ba4b099
Abel Kalum <chumbabel@gmail.com>
2020-01-25 19:42:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,5 @@
 # Write your code here.
 
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 def line
   katz_deli = []
   puts "The line is currently empty."
