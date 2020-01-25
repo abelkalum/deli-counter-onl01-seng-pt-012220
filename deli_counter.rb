9638b063bb8c4c42aa778e203b1ec8c79b32e67a
Abel Kalum <chumbabel@gmail.com>
2020-01-25 19:46:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,14 @@
 # Write your code here.
 
-def line
-  katz_deli = []
-  puts "The line is currently empty."
-end
-  
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
